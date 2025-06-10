65ef45ee9ed8f3145ebaaf8b394d0be0ae4ad646
git <stian.larsen@sparebank1.no>
2025-06-10 08:36:44 +0200

    stuff

--- src/app/actions/user/getBasicUser.ts
+++ src/app/actions/user/getBasicUser.ts
@@ -1,9 +1,8 @@
 "use server";
 import { UserBasic } from "@/app/actions/user/types";
-import { API_USER_BASIC_URL, ROUTE_SIGN_UP } from "@/utils/urls";
+import { API_USER_BASIC_URL } from "@/utils/urls";
 import { HTTP_REQUEST } from "@/utils/fetch/fetch";
 import { fetchWithAuth } from "@/utils/fetch/fetchWithAuth";
-import { redirect } from "next/navigation";
 import { CacheKeys } from "@/app/lib/cache/keys";
 
 export const getBasicUser = async () => {
@@ -16,7 +15,7 @@ export const getBasicUser = async () => {
 
   if (user.error) {
     console.error("Failed to get basic user data", user.error);
-    return redirect(ROUTE_SIGN_UP);
+    return null;
   }
 
   return user.data;
