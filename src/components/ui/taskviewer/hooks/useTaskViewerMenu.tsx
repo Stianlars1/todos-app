b2f0645849f19660898117f41ade4a46845fbd44
git <stian.larsen@sparebank1.no>
2025-06-11 08:03:12 +0200

    taskviewer and draggabletask update

--- src/components/ui/taskviewer/hooks/useTaskViewerMenu.tsx
+++ src/components/ui/taskviewer/hooks/useTaskViewerMenu.tsx
@@ -1,11 +1,19 @@
 import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const useTaskViewerMenu = () => {
+  const [isOpeningTask, setIsOpeningTask] = useState(false);
   const pathName = usePathname();
   const router = useRouter();
   const onTaskClick = (todoId: number) => {
-    router.push(`${pathName}?selectedTask=${todoId}`, undefined);
+    console.log("onTaskClick", todoId);
+    setIsOpeningTask(true);
+    try {
+      router.replace(`${pathName}?selectedTask=${todoId}`, undefined);
+    } finally {
+      setIsOpeningTask(false);
+    }
   };
 
-  return { onTaskClick };
+  return { onTaskClick, isOpeningTask };
 };
